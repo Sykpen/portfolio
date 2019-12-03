ec6260c9284cc5433d5ff459e6a9f4090b7ab5d1
Sykpen <ser10potap@yandex.ru>
2019-12-03 15:01:48 +0300

    add: previos projects

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-let swiper = new Swiper('.swiper-container');
+let swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
